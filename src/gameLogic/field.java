319d58553a2a1743da6cdb6c1e919970f53814e4
MrManiax <MrMan1ax@aol.de>
2015-03-17 12:16:31 +0100

    Update field.java

--- src/gameLogic/field.java
+++ src/gameLogic/field.java
@@ -2,9 +2,13 @@ package gameLogic;
 
 public class field {
 	private char type;
+	private int x;
+	private int y;
 	
-	public field (char type) {
+	public field (char type, int x, int y) {
 		this.type = type;
+		this.x = x;
+		this.y = y;
 	}
 
 	public char getType() {
@@ -13,5 +17,10 @@ public class field {
 
 	public void setType(char type) {
 		this.type = type;
+	}
+	
+	public int[] getPos() {
+		int[] tmp = [this.x, this.y];
+		return tmp;
 	}
 }
